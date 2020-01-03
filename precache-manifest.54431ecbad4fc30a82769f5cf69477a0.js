3c964758338b6d933eed7c85b3d61aba0f95dc01
Alex teshome <alex_teshome@hotmail.com>
2020-01-03 17:34:32 -0500

    Updates

---
+++ precache-manifest.54431ecbad4fc30a82769f5cf69477a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e72a115b56934709fc1a89f5e0d48d",
+    "url": "/alexteshome/ATMDb/index.html"
+  },
+  {
+    "revision": "ef09892c210e409a0bb7",
+    "url": "/alexteshome/ATMDb/static/css/main.afa88e5f.chunk.css"
+  },
+  {
+    "revision": "08ef1a7dd5d3b6a55efd",
+    "url": "/alexteshome/ATMDb/static/js/2.9fe68a7e.chunk.js"
+  },
+  {
+    "revision": "1d8a91108f8a09a09ace3b5b5338b228",
+    "url": "/alexteshome/ATMDb/static/js/2.9fe68a7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef09892c210e409a0bb7",
+    "url": "/alexteshome/ATMDb/static/js/main.60ea9866.chunk.js"
+  },
+  {
+    "revision": "dd268deb24d09b4acb46",
+    "url": "/alexteshome/ATMDb/static/js/runtime-main.b83d1e0c.js"
+  }
+]);
