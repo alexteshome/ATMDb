e38f9610c603951537b7fbb934d68f354710c9a6
Alex teshome <alex_teshome@hotmail.com>
2020-01-03 17:38:07 -0500

    github pages

--- src/actions/getMovies.js
+++ src/actions/getMovies.js
@@ -1,8 +1,7 @@
 import { GET_MOVIES, GET_ERRORS } from "./types";
 
 const fetchPopMovies = async () => {
-  //ENTER YOUR API KEY HERE:
-  //const API_Key = "";
+  const API_Key = "6c741e9f6eef00067e5bc6af8c6b9e01";
   const movieListRaw = await fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=${API_Key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
   );
