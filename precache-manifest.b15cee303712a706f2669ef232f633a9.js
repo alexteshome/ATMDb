0454e0e4a45a2e44d28711effce5676938dfecd6
Alex teshome <alex_teshome@hotmail.com>
2020-01-06 23:34:35 -0500

    Updates

---
+++ precache-manifest.b15cee303712a706f2669ef232f633a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b766e3931074899513ede6b1e7132a",
+    "url": "/ATMDb/index.html"
+  },
+  {
+    "revision": "895d06052d2fbc3235ee",
+    "url": "/ATMDb/static/css/main.afa88e5f.chunk.css"
+  },
+  {
+    "revision": "08ef1a7dd5d3b6a55efd",
+    "url": "/ATMDb/static/js/2.9fe68a7e.chunk.js"
+  },
+  {
+    "revision": "1d8a91108f8a09a09ace3b5b5338b228",
+    "url": "/ATMDb/static/js/2.9fe68a7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "895d06052d2fbc3235ee",
+    "url": "/ATMDb/static/js/main.07ba4648.chunk.js"
+  },
+  {
+    "revision": "f87b2738f9e1d6bbda5f",
+    "url": "/ATMDb/static/js/runtime-main.f29a4ba8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alexteshome/ATMDb/precache-manifest.54431ecbad4fc30a82769f5cf69477a0.js"
+  "/ATMDb/precache-manifest.b15cee303712a706f2669ef232f633a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alexteshome/ATMDb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ATMDb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
